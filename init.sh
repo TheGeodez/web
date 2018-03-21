3fecb09f86afc0da1cdd67c2f6ebc26d716c32ee
TheGeodez <34858157+TheGeodez@users.noreply.github.com>
2018-03-21 13:46:22 +0300

    Update init.sh
     настройте Gunicorn таким образом, что бы он запускал приложение  /home/box/web/hello.py , и принимал соединения на IP адресе 0.0.0.0 на порту 8080

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+gunicorn -b 0.0.0.0:8080 hello:hello_app.  (Использования IP = 0.0.0.0 необходимо для тестирования)
