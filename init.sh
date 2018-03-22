c0988007a1b5ddc933aee4de80ec58f3dd533df8
TheGeodez <34858157+TheGeodez@users.noreply.github.com>
2018-03-22 09:25:42 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-gunicorn -b 0.0.0.0:8080 hello:hello_app
+gunicorn -b 0.0.0.0:8000 ask.wsgi:application
