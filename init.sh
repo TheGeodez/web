c424ecca2e064767ee593c82fa0a13003ccb2c10
TheGeodez <34858157+TheGeodez@users.noreply.github.com>
2018-03-22 09:47:48 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorndjango.conf /etc/gunicorn.d/testdj
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/testdj
 sudo /etc/init.d/gunicorn restart
