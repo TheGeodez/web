6e34c0ddcd66b54183f16d6e708283741eaadbbf
TheGeodez <34858157+TheGeodez@users.noreply.github.com>
2018-04-09 13:56:09 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,22 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Question(models.Model):
-    title = models.CharField(max_length= 500)
-    text = models.TextField()
-    added_at = models.DateField(blank=True)
-    rating = models.IntegerField()
-    author = models.CharField(max_length=150)
-    likes = models.ManyToManyField( User, related name = 'question_like_user')
-    objects = QuestionManager()
-
-
 class QuestionManager(models.Manager):
     def new(self):
         return self.order_by('-added_at')
 
     def popular(self):
         return self.order_by('-rating')
+
+    
+class Question(models.Model):
+    objects = QuestionManager()
+    title = models.CharField(max_length= 500)
+    text = models.TextField()
+    added_at = models.DateField(blank=True)
+    rating = models.IntegerField()
+    author = models.CharField(max_length=150)
+    likes = models.ManyToManyField( User, related name = 'question_like_user')
+    
+
+
+
 
 
 class Answer(models.Model):
