af2590c46b1769ecd557cd1a3c7fb3af27e19ead
TheGeodez <34858157+TheGeodez@users.noreply.github.com>
2018-04-09 13:57:48 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -13,8 +13,8 @@ class Question(models.Model):
     objects = QuestionManager()
     title = models.CharField(max_length= 500)
     text = models.TextField()
-    added_at = models.DateField(blank=True)
-    rating = models.IntegerField()
+    added_at = models.DateField(blank=True, auto_now_add = True)
+    rating = models.IntegerField(default = 0)
     author = models.CharField(max_length=150)
     likes = models.ManyToManyField( User, related name = 'question_like_user')
     
